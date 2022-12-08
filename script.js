f5856f97048198baf0fad9e70cef9db817568c97
TaisYuri <taisyuri@hotmail.com>
2022-12-07 22:33:50 -0300

    add functions js

--- script.js
+++ script.js
@@ -45,3 +45,46 @@ menu.classList.replace('ativo', 'inativo');
 if(menu.classList.contains('ativo')){ //retorna boolean se contem a classe passada
     menu.classList.add('azul');
 }
+
+// verifique se as imagens possuem o atributo alt
+const imagens = document.querySelectorAll('img');
+imagens.forEach( item => {
+    const possuiAtr = item.hasAttribute('alt');
+    console.log(possuiAtr)
+})
+
+
+// ---------------------------------------------------------------------
+//captura tamanhos
+const lista = document.querySelector('.list-animals');
+
+const height = lista.scrollHeight // height + scroll
+const height2 = lista.offsetHeight // height + scroll
+const left = lista.offsetLeft // distancia da esquerda para inicio
+
+const rect = lista.getBoundingClientRect();  //coordenada com valores de width, height e posição (left, right)
+
+console.log(window.innerWidth)  //tamanho da janela
+console.log(window.outerHeight) //soma a barra de endereço
+console.log(window.scrollY)  //distancia total do scroll vertical
+
+const small = window.matchMedia('(max-width: 600px)');
+
+
+// ONLOAD renderiza apenas após o carregamento da pagina [ por padrão o js executa mais rapido do que a montagem da pagina]
+window.onload = function(){
+  const imagens = document.querySelectorAll('img')
+  let somaWidth = 0;
+  imagens.forEach( (image) => {
+    somaWidth += somaWidth + image.offsetWidth
+  })
+  console.log('soma', somaWidth)
+}
+
+// se o tamanho da pagina for menor que 600px, deve-se adicionar classe "menu-mobile"
+const browserSmall = window.matchMedia('(max-width: 600px)').matches  //retorna boolean
+
+if (browserSmall){
+  const menu = document.querySelector('.menu');
+  menu.classList.add('menu-mobile')
+}
